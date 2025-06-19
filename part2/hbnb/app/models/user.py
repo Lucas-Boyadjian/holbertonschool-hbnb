e3f2bd1ce98d8c70cc36af2aa0bb8dc6a4559916
JulienGirardey <juliengirardey2@gmail.com>
2025-06-19 14:15:46 +0200

    Refactor user model and API responses to use to_dict method for consistency

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -5,6 +5,10 @@ from .basemodel import BaseModel
 class User(BaseModel):
     def __init__(self, first_name, last_name, email, is_admin=False):
         super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.is_admin = is_admin
         if not first_name or len(first_name) > 50:
             raise ValueError("Invalid first name")
         if not last_name or len(last_name) > 50:
@@ -12,11 +16,15 @@ class User(BaseModel):
         if not email or '@' not in email:
             raise ValueError("Invalid email")
 
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.is_admin = is_admin
-        self.places = []
+    def to_dict(self):
+        return {
+            "id_user": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "is_admin": self.is_admin,
+            "places": []
+        }
 
     def add_place(self, place):
         self.places.append(place)
