1601e7e26f95f86fad5068ead07f235d3de54676
Yadjian92 <10462@holbertonstudents.com>
2025-06-19 14:57:33 +0200

    Refactor User model to remove place attribute and initialize places as an empty list

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -3,13 +3,13 @@
 from .basemodel import BaseModel
 
 class User(BaseModel):
-    def __init__(self, first_name, last_name, email, is_admin=False, place=[]):
+    def __init__(self, first_name, last_name, email, is_admin=False):
         super().__init__()
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.is_admin = is_admin
-        self.place = place
+        self.places = []
         if not first_name or len(first_name) > 50:
             raise ValueError("Invalid first name")
         if not last_name or len(last_name) > 50:
